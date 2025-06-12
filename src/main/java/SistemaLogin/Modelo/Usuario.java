d12339bf16392d83d4c7b7b475bc66f061bd56b6
Eduardo <ewaldo.jefe@gmail.com>
2025-06-11 21:49:08 -0400

    Integración 1206 Usuario|

--- src/main/java/SistemaLogin/Modelo/Usuario.java
+++ src/main/java/SistemaLogin/Modelo/Usuario.java
@@ -1,20 +1,20 @@
 package SistemaLogin.Modelo;
+
 /**
  * Clase que representa a un usuario del sistema.
  */
 public class Usuario {
     private String nombre;
     private String clave;
+    private Perfil perfil;
 
     /**
-     * Constructor que inicializa los atributos del usuario.
-     *
-     * @param nombre nombre del usuario
-     * @param clave clave del usuario
+     * Inicializa los atributos del usuario.
      */
-    public Usuario(String nombre, String clave) {
+    public Usuario(String nombre, String clave, String correo) {
         this.nombre = nombre;
         this.clave = clave;
+        this.perfil = new Perfil(correo);
     }
 
     public String getNombre() {
@@ -25,7 +25,15 @@ public class Usuario {
         return clave;
     }
 
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
     public void setClave(String clave) {
         this.clave = clave;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
     }
 }
